3b04352dbe78e823941b18fdbc141731fea98b64
Tech-A12 <Tech-A12@Tech-A12s-MacBook-Air.local>
2017-07-11 10:52:50 +0100

    Restful routes added

--- controllers/positions_controller.rb
+++ controllers/positions_controller.rb
@@ -1,4 +1,39 @@
 class PositionsController < Sinatra::Base
 
 
+	#INDEX
+	get "/" do
+		
+	end
+
+	#SHOW
+	get "/:id" do
+
+	end
+
+	#NEW
+	get "/new" do
+
+	end
+
+	#CREATE
+	post "/" do
+
+	end
+
+	#EDIT
+	get "/:id/edit" do
+
+	end
+
+	#UPDATE
+	put "/:id" do
+
+	end
+
+	#DELETE
+	delete "/:id" do
+
+	end
+
 end
